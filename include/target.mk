39ee9a6ed743f6eeb742ad475b3e7e1fd38cc8fa
u0225 <qq3842126@gmail.com>
2020-06-10 05:29:45 +0800

    修改target/x86默认包 增加/移除部分插件

--- include/target.mk
+++ include/target.mk
@@ -14,9 +14,12 @@ DEVICE_TYPE?=router
 
 # Default packages - the really basic set
 DEFAULT_PACKAGES:=base-files libc libgcc busybox dropbear mtd uci opkg netifd fstools uclient-fetch logd urandom-seed urngd luci luci-compat wget \
+block-mount coremark kmod-nf-nathelper kmod-nf-nathelper-extra kmod-ipt-raw libustream-openssl ca-certificates \
 default-settings luci-app-ddns luci-app-wol luci-app-vlmcsd luci-app-ramfree luci-app-flowoffload \
 luci-app-passwall luci-app-timecontrol luci-app-control-timewol luci-app-control-webrestriction luci-app-control-weburl luci-app-fileassistant \
-luci-app-pptp-vpnserver-manyusers luci-app-pppoe-server luci-app-pppoe-relay luci-theme-bootstrap-mod luci-theme-darkmatter
+luci-app-pptp-vpnserver-manyusers luci-app-pppoe-server luci-app-pppoe-relay luci-theme-bootstrap-mod luci-theme-darkmatter \
+luci-app-sfe luci-app-nlbwmon luci-app-accesscontrol luci-app-cpufreq \
+ddns-scripts_aliyun ddns-scripts_dnspod
 # For nas targets
 DEFAULT_PACKAGES.nas:=block-mount fdisk lsblk mdadm
 # For router targets
